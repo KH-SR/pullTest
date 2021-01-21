7894f9696a7b3473a3749aadb76f57f53727fdce
KH-SR <yourname1224@gmail.com>
2021-01-21 16:50:44 +0900

    Initial Commit

---
+++ WebViewFireabaseTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WebViewFireabaseTest
+//
+//  Created by USER on 1/21/21.
+//  Copyright © 2021 USER. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
